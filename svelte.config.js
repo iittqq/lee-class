80028ec8f786bb2cfb841d56b7fdf17a8e07740d
ikaika <leeikaika4@gmail.com>
2025-08-23 14:12:12 -0500

    test

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,11 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const dev = process.env.NODE_ENV === 'development';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ fallback: 'index.html' }) },
-	paths: {
-		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-	}
+	kit: { adapter: adapter({ fallback: 'index.html' }) }
 };
 
 export default config;
