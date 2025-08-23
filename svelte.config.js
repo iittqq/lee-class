0249c5ffcc18f0385bf204595442b346928d014d
ikaika <leeikaika4@gmail.com>
2025-08-23 14:28:28 -0500

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+// svelte.config.js
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
+export default {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ fallback: 'index.html' }) }
+	kit: {
+		adapter: vercel() // default serverless on Vercel
+	}
 };
-
-export default config;
